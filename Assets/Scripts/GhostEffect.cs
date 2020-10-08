e02be67c4267076de48e6a26e4c1dc0cce25d014
PazBazak <efratbazak2@gmail.com>
2020-10-09 00:08:48 +0300

    Fixed rotation
    * Fixed spawned ghost rotation
    * Added comments
    * Added dyanmic sprite

--- Assets/Scripts/GhostEffect.cs
+++ Assets/Scripts/GhostEffect.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 
 public class GhostEffect : MonoBehaviour
 {
-
+    // delay between ghost instantiation
     public float ghostDelay;
+
+    // ghost instantiation timer
     private float ghostDelaySeconds;
+
+    // actual ghost prefab to be displayed
     public GameObject ghostPrefab;
+
+    // parent ghosts objects
     public Transform GhostsParent;
 
+    // defining if it should create ghosts
     public bool makeGhost = false;
+
+    private float animationTime = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // starting the timer
         ghostDelaySeconds = ghostDelay;
     }
 
@@ -31,8 +41,21 @@ public class GhostEffect : MonoBehaviour
             else // genereate ghost
             {
                 GameObject currentGhost = Instantiate(ghostPrefab, transform.position, transform.rotation);
+
+                // get the sprite of the current player
+                Sprite currentSprite = GetComponent<SpriteRenderer>().sprite;
+
+                currentGhost.GetComponent<SpriteRenderer>().sprite = currentSprite;
                 currentGhost.transform.SetParent(GhostsParent);
+
+                // setting the rotation of the ghost same as the player ( *-1 seems like a temporary solution, until further issues of re-doing our images)
+                currentGhost.transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y * -1, transform.localScale.y);
+
+                // reseting the timer
                 ghostDelaySeconds = ghostDelay;
+
+                // Destroying the ghost after set time
+                Destroy(currentGhost, animationTime);
             }
         }
     }
